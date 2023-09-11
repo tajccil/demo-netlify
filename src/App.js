7c5326600a54835087c690221a0d629249fa7ee5
tajccil <taj.shaik@cortexcraft.com>
2023-09-11 16:03:04 +0530

    add js modified

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is Shaik, Learning React
         </a>
       </header>
     </div>
